0f57decb45a6f97f1eded9fc921bf3d293d78a44
Michael Rykov <mrykov@gmail.com>
2021-05-26 13:54:18 +0800

    Added progress bar for “push” command

--- cli/push.go
+++ cli/push.go
@@ -1,15 +1,27 @@
 package cli
 
 import (
+	"github.com/cheggaaa/pb/v3"
+	"github.com/gemfury/cli/api"
+	"github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
 
+const (
+	// Progress bar template to match legacy Gemfury CLI
+	pbTemplate pb.ProgressBarTemplate = `{{string . "prefix"}}{{ bar . "[" "=" (cycle . "⠁" "⠂" "⠄" "⠂") " " "]" }} {{percent . }}`
+)
+
 // NewCmdPush generates the Cobra command for "push"
 func NewCmdPush() *cobra.Command {
+	var noProgress bool
+
 	pushCmd := &cobra.Command{
 		Use:   "push PACKAGE",
 		Short: "Upload a new version of a package",
@@ -24,7 +36,15 @@ func NewCmdPush() *cobra.Command {
 				return err
 			}
 
+			// Initialize progress bar template
+			pbFactory := pb.ProgressBarTemplate(pbTemplate)
+
+			// Upload each file and collect errors
+			var multiErr *multierror.Error
 			for _, path := range args {
+				name := filepath.Base(path)
+				prefix := fmt.Sprintf("Uploading %s ", name)
+
 				err := func() error {
 					file, err := os.Open(path)
 					if err != nil {
@@ -32,24 +52,55 @@ func NewCmdPush() *cobra.Command {
 					}
 					defer file.Close()
 
-					name := filepath.Base(file.Name())
-					err = c.PushPkg(cc, name, file)
-					if err != nil {
-						return err
+					// Prepare progress bar
+					var reader io.Reader = file
+					if noProgress {
+						fmt.Print(prefix)
+						prefix = ""
+					} else {
+						stat, _ := file.Stat()
+						bar := pbFactory.Start64(stat.Size())
+						bar = bar.Set("prefix", prefix)
+						bar = bar.Set(pb.CleanOnFinish, true)
+						reader = bar.NewProxyReader(file)
+						defer bar.Finish()
 					}
 
-					fmt.Printf("Uploaded package %q\n", name)
-					return nil
+					err = c.PushPkg(cc, name, reader)
+					return err
 				}()
 
 				if err != nil {
-					return err
+					multiErr = multierror.Append(multiErr, err)
+				}
+
+				if err == nil {
+					fmt.Printf("%s- done\n", prefix)
+				} else if os.IsNotExist(err) {
+					fmt.Printf("%s- file not found\n", prefix)
+				} else if errors.Is(err, api.ErrClientAuth) {
+					fmt.Printf("%s- unauthorized\n", prefix)
+				} else if err.Error() == "Conflict" {
+					fmt.Printf("%s- this version already exists\n", prefix)
+				} else {
+					fmt.Printf("%s- error %q\n", prefix, err.Error())
 				}
 			}
 
-			return nil
+			if multiErr != nil {
+				cmd.SilenceUsage = true
+				cmd.SilenceErrors = true
+				multiErr.ErrorFormat = func([]error) string {
+					return "There was a problem uploading at least 1 package"
+				}
+			}
+
+			return multiErr.ErrorOrNil()
 		},
 	}
+
+	// Flags and options
+	pushCmd.Flags().BoolVar(&noProgress, "quiet", false, "Do not show progress bar")
 
 	return pushCmd
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d
+	github.com/cheggaaa/pb/v3 v3.0.8
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
 )
