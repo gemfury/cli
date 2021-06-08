711eda879c41ad24136654c75a0f73cb065f0a0a
Michael Rykov <mrykov@gmail.com>
2021-06-08 13:11:11 +0800

    Added “sharing” command to list members

--- api/sharing.go
+++ api/sharing.go
@@ -16,3 +16,30 @@ func (c *Client) RemoveCollaborator(cc context.Context, name string) error {
 	req := c.newRequest(cc, "DELETE", "/collaborators/"+url.PathEscape(name), true)
 	return req.doJSON(nil)
 }
+
+// Members returns the details of the collaborator listing
+func (c *Client) Members(cc context.Context, body *PaginationRequest) (*MembersResponse, error) {
+	req := c.newRequest(cc, "GET", "/members", true)
+
+	if body != nil {
+		c.prepareJSONBody(req, body)
+	}
+
+	resp := MembersResponse{}
+	pagination, err := req.doPaginatedJSON(&resp.Members)
+	resp.Pagination = pagination
+
+	return &resp, err
+}
+
+// MembersResponse represents details from Members API call
+type MembersResponse struct {
+	Pagination *PaginationResponse
+	Members    []*Member
+}
+
+// Member represents Member JSON
+type Member struct {
+	Role string `json:"role"`
+	AccountResponse
+}
--- cli/sharing.go
+++ cli/sharing.go
@@ -1,10 +1,13 @@
 package cli
 
 import (
+	"github.com/gemfury/cli/api"
 	"github.com/spf13/cobra"
 
 	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 )
 
 // Root for sharing/collaboration subcommands
@@ -12,12 +15,52 @@ func NewCmdSharingRoot() *cobra.Command {
 	gitCmd := &cobra.Command{
 		Use:   "sharing",
 		Short: "Collaboration commands",
+		RunE:  listMembers,
 	}
 
 	gitCmd.AddCommand(NewCmdSharingAdd())
 	gitCmd.AddCommand(NewCmdSharingRemove())
 
 	return gitCmd
+}
+
+func listMembers(cmd *cobra.Command, args []string) error {
+	cc := cmd.Context()
+	c, err := newAPIClient(cc)
+	if err != nil {
+		return err
+	}
+
+	members := []*api.Member{}
+
+	// Paginate over package listings until no more pages
+	err = iterateAllPages(cc, func(pageReq *api.PaginationRequest) (*api.PaginationResponse, error) {
+		resp, err := c.Members(cc, pageReq)
+		if err != nil {
+			return nil, err
+		}
+
+		members = append(members, resp.Members...)
+		return resp.Pagination, nil
+	})
+
+	// Handle no packages
+	if len(members) == 0 {
+		fmt.Println("No members found for this account")
+		return nil
+	}
+
+	// Print results
+	fmt.Printf("*** Collaborators ***\n")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	fmt.Fprintf(w, "name\trole\n")
+
+	for _, m := range members {
+		fmt.Fprintf(w, "%s\t%s\n", m.Name, m.Role)
+	}
+
+	w.Flush()
+	return nil
 }
 
 // NewCmdSharingAdd generates the Cobra command for "sharing:add"
