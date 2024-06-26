c46e49f60990914d4319d31c3a8d5f4411288d59
Michael Rykov <mrykov@gmail.com>
2024-06-26 20:44:01 +0800

    Fixed timeStringWithAgo for sub-minute age

--- cli/helpers.go
+++ cli/helpers.go
@@ -14,7 +14,8 @@ func timeStringWithAgo(t time.Time) string {
 	out := t.Local().Format("2006-01-02 15:04")
 
 	if ago := time.Now().Sub(t); ago < 24*time.Hour {
-		if str := roundDurationRE.FindString(ago.String()); str != "" {
+		agoStr := ago.Round(time.Second).String()
+		if str := roundDurationRE.FindString(agoStr); str != "" {
 			out = fmt.Sprintf("%s (~ %s ago)", out, str)
 		}
 	}
